936d4be38a5b7ade7d5b6685fe68da4f32282c0a
Josh Gao <josh@insolit.us>
2013-10-09 00:38:54 -0700

    Convert metamove.cpp to ObjC++ in preparation for daemonizing it.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-clang++ -Wall -Wextra -Werror -std=c++11 -stdlib=libc++ -O3 -lc++ -framework ApplicationServices -framework CoreFoundation -o metamove src/metamove.cpp
+clang++ -Wall -Wextra -Werror -std=c++11 -stdlib=libc++ -O3 -lc++ -framework ApplicationServices -framework CoreFoundation -o metamove src/metamove.mm
