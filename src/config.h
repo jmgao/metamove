e968c766c2d09f072ed470311f87b62c42ddead7
Josh Gao <josh@insolit.us>
2013-11-16 02:55:28 -0800

    Use preprocessed Info.plist for app bundle versioning.

--- src/config.h
+++ src/config.h
@@ -19,22 +19,11 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include <ApplicationServices/ApplicationServices.h>
-#include "config.h"
+#define VERSION 0.3.0
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 3
+#define VERSION_PATCH 0
 
-enum class config_mouse_button {
-    unknown = 0,
-    disabled,
-    left,
-    right
-};
-
-config_mouse_button get_move_button(void);
-config_mouse_button get_resize_button(void);
-void set_move_button(config_mouse_button button);
-void set_resize_button(config_mouse_button button);
-
-CGEventMask get_move_modifiers(void);
-CGEventMask get_resize_modifiers(void);
-void set_move_modifiers(CGEventMask modifiers);
-void set_resize_modifiers(CGEventMask modifiers);
+#define STRINGIFY_(x) #x
+#define STRINGIFY(x) STRINGIFY_(x)
+#define VERSION_STRING STRINGIFY(VERSION_MAJOR) "." STRINGIFY(VERSION_MINOR) "." STRINGIFY(VERSION_PATCH)
