847f0e0c9ab6260a2dbb525883826be434ce9eca
Josh Gao <josh@insolit.us>
2015-07-09 16:10:08 -0700

    Bump version number to 0.3.7.

--- src/config.h
+++ src/config.h
@@ -19,10 +19,10 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#define VERSION 0.3.6
+#define VERSION 0.3.7
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 3
-#define VERSION_PATCH 6
+#define VERSION_PATCH 7
 
 #define STRINGIFY_(x) #x
 #define STRINGIFY(x) STRINGIFY_(x)
